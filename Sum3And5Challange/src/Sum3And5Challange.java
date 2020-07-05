0eada257fb1a3fa29ca172d1b0cfd660ddf3b775
Adamos <34448203+stavrinoua@users.noreply.github.com>
2020-07-05 23:37:07 +0300

    progress

---
+++ Sum3And5Challange/src/Sum3And5Challange.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Sum3And5Challange {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> savedNumbers = new ArrayList<>();
+
+        int sum = 0;
+        for (int i = 0; i <= 1000; i++) {
+
+            if (savedNumbers.size() == 5) {
+                break;
+            }
+
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                savedNumbers.add(i);
+            }
+        }
+
+        for(int i = 0; i < savedNumbers.size(); i++) {
+            sum += savedNumbers.get(i);
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
