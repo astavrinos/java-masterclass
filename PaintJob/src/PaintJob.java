260a80783f1f2eb9765472d4f8758cc0fafc4204
Adamos <34448203+stavrinoua@users.noreply.github.com>
2020-10-21 20:34:08 +0300

    progress

--- PaintJob/src/PaintJob.java
+++ PaintJob/src/PaintJob.java
@@ -1,6 +1,3 @@
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-
 public class PaintJob {
 
     public static void main(String[] args) {
@@ -11,27 +8,34 @@ public class PaintJob {
         System.out.println(getBucketCount(-3.4, 2.1, 1.5));
         System.out.println(getBucketCount(3.4, 2.1, 1.5));
         System.out.println(getBucketCount(7.25, 4.3, 2.35));
+        System.out.println("-----");
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets <= 0) {
+        if (width <= 0.0 || height <= 0.0 || areaPerBucket <= 0.0 || extraBuckets < 0) {
             return -1;
         }
 
-        double area = width * height;
-        double bucketsToBeUsed = area / areaPerBucket;
-
-        return (int) (Math.round(bucketsToBeUsed) - extraBuckets);
+        return (int) Math.ceil((width * height) / areaPerBucket) - extraBuckets;
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+        if (width <= 0.0 || height <= 0.0 || areaPerBucket <= 0.0) {
             return -1;
         }
 
-        double area = width * height;
-        double bucketsToBeUsed = area / areaPerBucket;
-        return (int) (Math.round(bucketsToBeUsed));
+        return (int) Math.ceil((width * height) / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0.0 || areaPerBucket <= 0.0) {
+            return -1;
+        }
+
+        return (int) Math.ceil(area / areaPerBucket);
     }
 
 }
