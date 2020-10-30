349f7e9413c084986ee3e7fb29f3186ffa2e4e81
Adamos <34448203+stavrinoua@users.noreply.github.com>
2020-10-31 01:07:42 +0200

    Done two different challenges

---
+++ MusicPlayerWithInnerClasses/src/Song.java
@@ -0,0 +1,19 @@
+public class Song {
+
+    private String title;
+    private int duration;
+
+    public Song(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Title " + title + " (" + duration + " sec)";
+    }
+}
